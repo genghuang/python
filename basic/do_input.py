3900e5a1df649418459a0e541e0d2bbbdd1c9573
lingenghuang <18859698900@163.com>
2019-12-26 10:45:59 +0800

    add some files

---
+++ basic/do_input.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+name = input()
+print(name)
+names = input("Pleace enter your name: ")
+print("hello", name)
+a = 3.5
+print(a * a)
+x = 1024
+y = 768
+print("x * y =", x * y)
